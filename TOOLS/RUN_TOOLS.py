c07476c103465aa5f8d18125309f3a7ffee67dfc
Eugene <tek94@mail.ru>
2023-11-06 20:27:08 -0800

    window docking base logic

--- TOOLS/RUN_TOOLS.py
+++ TOOLS/RUN_TOOLS.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+def onMayaDroppedPythonFile(*args, **kwargs):
+	# scriptPath = # TODO get directory path and set them as main path
+	print("TODO| dropped python file empty function")
+
+
 scriptPath = "C:\MyFiles\Personal\Repositories\GETools\TOOLS"
 
 if not os.path.exists(scriptPath):
@@ -11,8 +16,8 @@ if scriptPath not in sys.path:
 
 
 ### TEST ZONE ###
-# import experimental.GeneralWindow as gtwindow
-# gtwindow.GeneralWindow().RUN()
+import experimental.GeneralWindow as gtwindow
+gtwindow.GeneralWindow().RUN()
 
 # import scripts.old_GETools as oldget
 # oldget.GETOOLS_class().Start()
@@ -22,9 +27,3 @@ if scriptPath not in sys.path:
 
 # import scripts.CenterOfMass as com
 # com.CenterOfMass().RUN()
-
-# import experimental.TESTING as testing
-# testing.TEST_PRINT_1()
-
-def onMayaDroppedPythonFile(*args, **kwargs):
-	print("TODO| dropped python file empty function")
--- TOOLS/experimental/TESTING.py
+++ TOOLS/experimental/TESTING.py
@@ -1,8 +1,44 @@
-def TEST_PRINT_1():
-	print("1 Test Hello World !!!")
+import maya.cmds as cmds
 
-def TEST_PRINT_2():
-	print("2 Test Hello World !!!")
+nameWindow = "qweqwe"
+windowWidth = 300
+windowHeight = 100
+margin = 5
+dockName = None
 
-def TEST_PRINT_3():
-	print("3 Test Hello World !!!")
+
+# Create a new window
+if cmds.window(nameWindow, exists = True):
+	cmds.deleteUI(nameWindow)
+
+cmds.window(nameWindow, title = "Label", maximizeButton = False, sizeable = True, widthHeight = (windowWidth, windowHeight))
+layout0 = cmds.columnLayout(adjustableColumn = True, width = windowWidth)
+
+
+# FRAME 1
+cmds.frameLayout(parent = layout0, label = "FRAME 1", collapsable = True, marginWidth = margin, marginHeight = margin)
+# countOffsets = 3
+# cmds.gridLayout(numberOfColumns = countOffsets, cellWidth = windowWidth / countOffsets)
+cmds.button(label = "test")
+cmds.button(label = "test")
+cmds.button(label = "test")
+
+# Show the window
+# cmds.showWindow(nameWindow)
+
+
+dockExists = cmds.dockControl("GETOOLS", query = True, exists = True)
+if dockExists:
+    print("Dock control 'GETOOLS' already exists.")
+else:
+    print("No dock control with the label 'GETOOLS' found.")
+
+
+
+
+allowedAreas = ['right', 'left']
+dockName = cmds.dockControl(label = "GETOOLS", area = 'left', content = nameWindow, allowedArea = allowedAreas, moveable = True)
+
+print(dockName)
+
+
