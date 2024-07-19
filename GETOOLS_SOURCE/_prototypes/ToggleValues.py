247770785a1a69d11302be672b2957120c48742d
Eugene <tek94@mail.ru>
2024-07-19 16:48:49 -0700

    Create ToggleValues.py

---
+++ GETOOLS_SOURCE/_prototypes/ToggleValues.py
@@ -0,0 +1,42 @@
+import maya.cmds as cmds
+
+toggleValue = (0, 1)
+
+cmds.select("test1:cube1", replace = True)
+
+# Extract namespace from selected
+selected = cmds.ls(sl = True)[0]
+resultNamespace = selected.split(':')[0]
+count = selected.split(':')
+
+if(len(count) < 2):
+	resultNamespace = None
+
+attributePath = selected + ".translateY"
+
+currentValue = cmds.getAttr(attributePath)
+resultValue = None
+
+if(currentValue < toggleValue[0]):
+	resultValue = toggleValue[0]
+
+elif(currentValue > toggleValue[1]):
+	resultValue = toggleValue[1]
+
+elif(currentValue > toggleValue[0] and currentValue < toggleValue[1]):
+	mathResult = currentValue - (toggleValue[1] - toggleValue[0]) / 2
+	if(mathResult <= 0):
+		resultValue = toggleValue[0]
+	else:
+		resultValue = toggleValue[1]
+
+elif(currentValue == toggleValue[0]):
+	resultValue = toggleValue[1]
+
+elif(currentValue == toggleValue[1]):
+	resultValue = toggleValue[0]
+
+cmds.setAttr(attributePath, resultValue)
+
+cmds.select(resultNamespace + ":" + "cube2", replace = True)
+		
